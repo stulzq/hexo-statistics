65255967184296e108b72ef036260baae94c1f2f
Zhiqiang Li <stulzq@qq.com>
2022-07-07 17:05:47 +0800

    fix bug

--- config/statistics.go
+++ config/statistics.go
@@ -4,7 +4,7 @@ func GetStatAllowSite() map[string]int {
 	source := Get("statistics:site").([]interface{})
 	allowSite := make(map[string]int, len(data))
 
-	for i := 0; i < len(data); i++ {
+	for i := 0; i < len(source); i++ {
 		allowSite[source[i].(string)] = 0
 	}
 
